b722aba51e032ea4ed8e187f1322d280bb4e76fa
Rahul Kulal <rahulkulala7@gmail.com>
2025-10-28 13:11:28 +0530

    Update GalleryServlet.java

--- src/main/java/com/gallery/GalleryServlet.java
+++ src/main/java/com/gallery/GalleryServlet.java
@@ -10,12 +10,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-// *** UPDATED TO JAKARTA EE IMPORTS ***
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+// *** SWITCHED TO JAVAX IMPORTS ***
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+// **********************************
+
+// ...
 // ************************************
 
 @WebServlet("/gallery")
